aed64b0a831a6c133bd63f6cc17b7014cdae6063
huangy <huangy@daoshuit.com>
2018-10-18 18:01:38 +0800

    canvas

--- src/components/MyVideo.vue
+++ src/components/MyVideo.vue
@@ -2,6 +2,7 @@
  <div id="video-wrapper"  ref="vdWrapper">
    <video id="my-video" class="video-js vjs-default-skin" controls preload="auto" >
     <source src="http://10.168.4.181:10001/hls/0100102001/0100102001_live.m3u8" type="application/x-mpegURL">
+    <!-- <source src="rtmp://10.168.4.181:10935/hls/0100102001" type="rtmp/flv"> -->
    </video>
     <canvas id="myCanvas" 
     ref="canvasWrapper"
@@ -11,7 +12,6 @@
     ></canvas>
  </div>
 </template>
-
 <script>
 import videojs from "video.js";
 import "videojs-contrib-hls";
@@ -20,8 +20,16 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      drag:false,
-      rect:{}
+      drag: false,
+      rect: {},
+      point: {
+        startX: "",
+        startY: "",
+        endX: "",
+        endY: "",
+        w: "",
+        h: ""
+      }
     };
   },
   mounted() {
@@ -48,21 +56,83 @@ export default {
       };
       this.$refs.canvasWrapper.setAttribute("width", vdSize.w);
       this.$refs.canvasWrapper.setAttribute("height", vdSize.h);
-      console.log(vdSize);
+      console.log("canvas的尺寸", vdSize);
     },
-    canvasDown(e){
-      let canvas = this.$refs.canvas;
-      let ctx = canvas.getContext("2d");
-      this.rect.startX = e.pageX - this.offsetLeft -8;
-      this.rect.startY = e.pageY - this.offsetTop - 8;
+    canvasDown(e) {
+      let myCanvas = this.$refs.canvasWrapper;
+      let ctx = myCanvas.getContext("2d");
+      this.point.startX = e.pageX - this.$refs.canvasWrapper.offsetLeft;
+      this.point.startY = e.pageY - this.$refs.canvasWrapper.offsetTop;
       this.drag = true;
-      console.log(this.rect.startX,this.rect.startY)
     },
-    canvasMove(e){
-
+    canvasMove(e) {
+      let myCanvas = this.$refs.canvasWrapper;
+      let ctx = myCanvas.getContext("2d");
+      this.drawScreen();
+      if (this.drag) {
+        this.point.w =
+          e.pageX - this.$refs.canvasWrapper.offsetLeft - this.point.startX;
+        this.point.h =
+          e.pageY - this.$refs.canvasWrapper.offsetTop - this.point.startY;
+        ctx.clearRect(0,0,this.$refs.canvasWrapper.width,this.$refs.canvasWrapper.height);
+        this.drawRect("stroke");
+      }
     },
-    canvasUp(e){
-
+    canvasUp(e) {
+      let myCanvas = this.$refs.canvasWrapper;
+      let ctx = myCanvas.getContext("2d");
+      this.point.endX = e.pageX - this.$refs.canvasWrapper.offsetLeft;
+      this.point.endY = e.pageY - this.$refs.canvasWrapper.offsetTop;
+      this.checkDirection();
+      console.log(this.point);
+      this.drag = false;
+      this.point = {
+        startX: "",
+        startY: "",
+        endX: "",
+        endY: "",
+        w: "",
+        h: ""
+      };
+      ctx.clearRect(0,0,this.$refs.canvasWrapper.width,this.$refs.canvasWrapper.height);
+    },
+    drawRect(style) {
+      let myCanvas = this.$refs.canvasWrapper;
+      let ctx = myCanvas.getContext("2d");
+      if (style === "fill") {
+        ctx.fillRect(
+          this.point.startX,
+          this.point.startY,
+          this.point.w,
+          this.point.h
+        );
+      }
+      if (style === "stroke") {
+        ctx.strokeRect(
+          this.point.startX,
+          this.point.startY,
+          this.point.w,
+          this.point.h
+        );
+      }
+    },
+    drawScreen() {
+      let myCanvas = this.$refs.canvasWrapper;
+      let ctx = myCanvas.getContext("2d");
+      ctx.lineWidth = 2;
+      ctx.fillStyle = "#b22acb";
+      ctx.strokeStyle = "#b22acb";
+    },
+    checkDirection() {
+      if (this.point.h < 0) {
+        console.log("这是缩小");
+        //  turnZoomOut();
+      } else if (this.point.startX === this.point.endX && this.point.startY === this.point.endY ) {
+        console.log("这是一个点");
+        // turnReset();
+      } else if (this.point.h > 0) {
+        console.log("这是扩大");
+      }
     }
   }
 };
